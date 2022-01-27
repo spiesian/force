e128a5b7cf0073f48f28273db881e9f4f18cc15d
Nikita Skalkin <nikskalkin@gmail.com>
2022-01-27 16:40:27 +0100

    [SWA-210]: comments cleanup

--- src/v2/Apps/Consign/Routes/SubmissionFlow/UploadPhotos/UploadPhotos.tsx
+++ src/v2/Apps/Consign/Routes/SubmissionFlow/UploadPhotos/UploadPhotos.tsx
@@ -128,7 +128,6 @@ export const UploadPhotos: React.FC<UploadPhotosProps> = ({ submission }) => {
                   input: {
                     assetType: "image",
                     geminiToken: photo.geminiToken!,
-                    // submissionID: submission.id,
                     externalSubmissionId: submission.externalId,
                     sessionID: !isLoggedIn ? getENV("SESSION_ID") : undefined,
                     filename: photo.name,
